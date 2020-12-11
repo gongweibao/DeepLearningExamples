19b8185d73ce9e979dbe3e4e5e12ef29b63c38ce
gongweibao <weibao.gong@gmail.com>
2020-12-11 03:38:33 -0800

    docker tag

--- TensorFlow/LanguageModeling/BERT/build_push.sh
+++ TensorFlow/LanguageModeling/BERT/build_push.sh
@@ -3,6 +3,6 @@ docker pull nvcr.io/nvidia/tritonserver:20.09-py3
 docker build . -t ${image}
 #docker push ${image}
 
-${inter_image}="registry.baidu.com/paddlepaddle-public/benchmark/ngc-bert:99b1"
+inter_image="registry.baidu.com/paddlepaddle-public/benchmark/ngc-bert:99b1"
 docker tag ${image} ${inter_image}
 #docker save > bert.tar
